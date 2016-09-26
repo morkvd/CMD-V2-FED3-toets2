c0f6f5d4a26716eba25107dc9b7e0bdec3f62f9d
morkvd <markvdijken@gmail.com>
2016-09-26 13:32:00 +0200

    draws an arc

--- main.js
+++ main.js
@@ -1,5 +1,32 @@
-const config = {};
+const pieWidth = 960;
 
-d3.csv('timetable.csv', data => {
+function cleanup(row) {
+  return {
+    activity: row.Activity,
+    location: row.Location,
+    duration: row.Duration,
+    endTime: row["End time"],
+    endDate: row["End date"],
+    endDay: row["End day"],
+    startTime: row["Start time"],
+    startDate: row["Start date"],
+    startDay: row["Start day"],
+    startWeek: row["Start week (ISO)"],
+  };
+}
+
+d3.csv('timetable.csv', cleanup, data => {
   console.log(data);
+
+  const pie = d3.select('svg');
+
+  var arc = d3.arc()
+    .innerRadius(50)
+    .outerRadius(70)
+    .startAngle(45 * (Math.PI/180)) //converting from degs to radians
+    .endAngle(3) //just radians
+
+  pie.append('path')
+    .attr("d", arc)
+    .attr("transform", "translate(200,200)");
 });
