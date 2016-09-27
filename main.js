46b305e7b5f631b554d0dd6e89b9484118f9565f
morkvd <markvdijken@gmail.com>
2016-09-27 23:21:51 +0200

    load location data

--- main.js
+++ main.js
@@ -1,35 +1,19 @@
-function cleanup(row) {
-  return {
-    activity: row.Activity,
-    location: row.Location,
-    duration: row.Duration,
-    endTime: row["End time"],
-    endDate: row["End date"],
-    endDay: row["End day"],
-    startTime: row["Start time"],
-    startDate: row["Start date"],
-    startDay: row["Start day"],
-    startWeek: row["Start week (ISO)"],
-  };
-}
+const marginTop = 40;
 
-d3.csv('timetable.csv', cleanup, data => {
+d3.csv('20160919locations.csv', data => {
   console.log(data);
-
-  const chart = d3.select('svg');
-
-  const [startDate, endDate] = d3.extent(data, entry => entry.startDate);
-  const [startYear, startMonth, startDay] = startDate.split('-');
-  const [endYear, endMonth, endDay] = endDate.split('-');
 
   const xScale = d3.scaleTime()
     .domain([
-      new Date(startYear, startMonth, startDay),
-      new Date(endYear, endMonth, endDay)
+      new Date(2016, 9, 19),
+      new Date(2016, 9, 20)
     ])
-    .range([0, 1000]);
+    .range([0, 1000])
+    .ticks(24);
 
   console.log(xScale);
-});
 
-// [1]: http://stackoverflow.com/questions/9229645/remove-duplicates-from-javascript-array
+  const chart = d3.select('svg');
+
+
+});
