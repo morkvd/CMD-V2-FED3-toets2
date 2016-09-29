dae985624550ac0f98213ab6fa02e05fb7938e55
morkvd <markvdijken@gmail.com>
2016-09-29 09:54:25 +0200

    added comments, rearranged code

--- main.js
+++ main.js
@@ -1,7 +1,7 @@
 /* global d3 */
 
 /* Mark van Dijken */
-d3.csv('20160919locations.csv', cleanDates, plot);
+d3.csv('20160919locations.csv', cleanUpData, plot);
 
 // settings object
 const config = {
@@ -24,16 +24,25 @@ const config = {
 };
 
 function plot(locationData) {
+
+  // (TODO: load dates from data instead of hard-coding it)
   const scaleX = d3.scaleTime()
-    .domain([ new Date('2016-09-19T00:00:00'), new Date('2016-09-20T00:00:00') ]) // make dynamic
+    .domain([ new Date('2016-09-19T00:00:00'), new Date('2016-09-20T00:00:00') ])
     .range([0, config.svg.width]);
 
-  function removeDuplicates(item, pos, arr) {
-    return !pos || item != arr[pos - 1];
-  }
-  // grab all labels from data, sort them, then remove all strings that are the same as their predecessor [2]
+  // grab all labels from data, sort them, then remove all duplicate stings
   const uniqueLabels = locationData.map(d => d.label).sort().filter(removeDuplicates);
+
+  // create a scale which maps all possible label values to `d3.schemeCategory10` colors
   const colorScale = d3.scaleOrdinal().domain(uniqueLabels).range(d3.schemeCategory10);
+
+  // output element that displays currently selected time
+  const timeSelectionDisplay = document.querySelector('#timeSelectionDisplay');
+
+  // input element that controls currently selected time
+  const timeSelectionControl = document.querySelector('#timeSelectionControl');
+
+  let timeSelectionLocation;
 
   const chart = d3.select('svg')
     .attr('class', 'chart')
@@ -42,10 +51,12 @@ function plot(locationData) {
       .append('g')
     .attr('transform', `translate(${ config.svg.margin.x / 2 }, 0)`);
 
+  // add x-axis
   chart.append('g')
     .attr('transform', `translate(0, ${config.svg.margin.y + config.bar.height + config.bar.margin})`)
     .call(d3.axisBottom(scaleX).ticks(24));
 
+  // setup range input for date selection
   d3.select('#timeSelectionControl')
     .attr('type', 'range')
     .attr('min', 0)
@@ -56,6 +67,11 @@ function plot(locationData) {
 
   drawTimeBlocks();
 
+  onTimeSelectionChange();
+
+  timeSelectionControl.addEventListener('input', onTimeSelectionChange);
+
+  // draw the timeSelectionIndicator over the timeBlocks previously drawn
   chart.append('rect')
     .attr('class', 'timeSelectionIndicator')
     .attr('width', 2)
@@ -63,19 +79,6 @@ function plot(locationData) {
     .attr('y', config.svg.margin.y / 2)
     .attr('height', 220)
     .attr('fill', 'red');
-
-  const timeSelectionDisplay = document.querySelector('#timeSelectionDisplay');
-  const timeSelectionControl = document.querySelector('#timeSelectionControl');
-
-  timeSelectionControl.addEventListener('input', onTimeSelectionChange);
-
-  function onTimeSelectionChange() {
-    const selectedDate = scaleX.invert(timeSelectionControl.value);
-    timeSelectionDisplay.value = selectedDate;
-    drawTimeBlocks(selectedDate);
-    chart.select('.timeSelectionIndicator')
-      .attr('transform', `translate(${ timeSelectionControl.value }, 0)`);
-  }
 
   function drawTimeBlocks() {
     const group = chart.selectAll('g .block').data(locationData);
@@ -100,9 +103,19 @@ function plot(locationData) {
 
     group.exit().remove();
   }
+
+  function onTimeSelectionChange() {
+    timeSelectionLocation = timeSelectionControl.value;
+    timeSelectionDisplay.value = scaleX.invert(timeSelectionLocation);
+
+    // translate timeSelectionIndicator to the position selected by the timeSelectionControl
+    chart.select('.timeSelectionIndicator')
+      .attr('transform', `translate(${ timeSelectionLocation }, 0)`);
+  }
 }
 
-function cleanDates(row) {
+// transforms dateTimeStings into js date objects
+function cleanUpData(row) {
   return {
     description: row.description,
     startDatetime: new Date(stripTimezone(row.startDatetime)),
@@ -112,8 +125,14 @@ function cleanDates(row) {
   };
 }
 
+// remove timezone info from datetimeString
 function stripTimezone(datetimeString) {
   return datetimeString.split('+')[0];
+}
+
+// filters out strings that are the same as their predecessor [2]
+function removeDuplicates(item, pos, arr) {
+  return !pos || item != arr[pos - 1];
 }
 
 // [1] http://stackoverflow.com/questions/24912274/d3-update-data-with-multiple-elements-in-a-group
